d588f9495306e9ccd59afa38c58a9f09b917f980
Matt Merchant <matthew.lee.merchant@gmail.com>
2024-03-20 21:04:36 -0500

    Create Day02Part1.sh

---
+++ 2021/02/Day02Part1.sh
@@ -0,0 +1,21 @@
+depth=0
+forward=0
+
+while read; do
+    command=$(echo $REPLY | cut -d ' ' -f 1)
+    increment=$(echo $REPLY | cut -d ' ' -f 2)
+    if [[ $command == "up" ]]; then
+        depth=$((depth - increment))
+    elif [[ $command == "down" ]]; then
+        depth=$((depth + increment))
+    elif [[ $command == "forward" ]]; then
+        forward=$((forward + increment))
+    else
+        echo "Unexpected Input"
+        exit 1
+    fi
+done < challenge.txt
+
+echo $((depth * forward))
+
+# 1690020
