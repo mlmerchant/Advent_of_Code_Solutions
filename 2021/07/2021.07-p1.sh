ebb276565671725e74f7ec69bfe495a5d443753f
Matthew Merchant <matthew.lee.merchant@gmail.com>
2024-04-10 14:39:29 -0500

    partial work for solving part 1

--- 2021/07/2021.07-p1.sh
+++ 2021/07/2021.07-p1.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
+input=challenge.txt
+
 # Get the most common number
-target=$(cat challenge.txt | sort -n | uniq -c | sort -n -k 1 | tail -n 1 | cut -d ' ' -f 8)
+target=$(cat $input | sort -n | uniq -c | sort -n -k 1 | tail -n 1 | cut -d ' ' -f 8)
+
+echo Target is $target.
+
+#absolute value function
+. ../../Tools/abs.sh
 
 
+total=0
+
+while read; do
+  position=$REPLY
+  echo "$position is the position"
+  moves=$(( target - position ))
+  echo "which is this distance from target: $moves"
+  moves=$(abs $moves)
+  echo "Which is this positive number: $moves"
+  total=$(( total + moves ))
+  echo "And here is the total: $total"
+done < $input
+
+echo $total
+
+# 398784 is too high
+# 398697 is too high
