0fa1b0e3785dd52a78f390134558ad93df5521dc
Matt Merchant <matthew.lee.merchant@gmail.com>
2025-12-03 19:30:14 -0600

    solved 2025, day 1, part 1

---
+++ 01-1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while IFS= read -r line; do
+  direction="${line:0:1}"
+  turns=${line:1}
+  if [ $direction == "L" ]; then
+      turns=$((turns * -1))
+  fi
+  needle=$((needle + turns))
+  needle_check=$((needle % 100))
+  if [ $needle_check -eq 0 ]; then
+      ((zeros++))
+  fi
+done < puzzle_01.txt
+
+needle=50
+zeros=0 
+echo $zeros
+
+#Answer is 1054
